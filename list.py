697ec699ee7f6cec778a9f5732079134591917f7
nageswau <nag0303koti@gmail.com>
2017-10-12 07:27:04 +0530

    11/10/2017

---
+++ list.py
@@ -0,0 +1,13 @@
+a  = []
+print a
+print type(a)
+a  = [1,2,3,'nag',5.5]
+print a
+print a[0]
+print a[4]
+print "negative indexing"
+print a[-1]
+print a[-3]
+#print a[-7]
+print list("python")
+print "python"
