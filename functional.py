8639dd73b6430e877ef3397a890b233f3ad649e2
nageswau <nag0303koti@gmail.com>
2017-11-03 10:03:02 +0530

    03112017

---
+++ functional.py
@@ -0,0 +1,15 @@
+
+
+def addi(a,b):
+    return a+b
+
+def subs(a,b):
+    return a-b
+
+def main():
+    print addi(3,5)
+    print subs(3,5)
+
+if __name__ == "__main__":
+    print __name__
+    main()
